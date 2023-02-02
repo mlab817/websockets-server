91fb0060b618c5cf542bbd4a2396cfc2e715e505
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-02-02 08:58:27 +0800

    updated echo config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,11 +26,11 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    key: '1b421e8d437e47b9eee3', // for websockets server default key
+    cluster: 'ap1',
+    wsHost: '127.0.0.1',
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
